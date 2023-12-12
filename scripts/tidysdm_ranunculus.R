d1b17d85d394db0eb0be9feff15e9fd4fd5c61af
hpilat <hpilat@student.ubc.ca>
2023-12-12 14:36:52 -0600

    updated script with Ranunculus inputted into tidysdm tutorial script

--- scripts/tidysdm_ranunculus.R
+++ scripts/tidysdm_ranunculus.R
@@ -1,21 +1,27 @@
 # tidysdm tutorial
 library(tidysdm)
 library(tidyterra)
+library(sf)
+library(pastclim)
 
 # read in Ranunculus glaberrimus presence dataframe, 
-  # dataframe with ID, latitude, and longitude columns turned to SpatVector
+  # dataframe with ID, latitude, and longitude columns
 ran_occ
 
-# create a SpatVector object for the occurrence data
-ran_occ_vect <- vect(ran_occ, geom = c("longitude", "latitude"), 
-                     crs = "EPSG:4326", keepgeom = FALSE)
+# plot the presences on a map to visualize them
+# cast coordinates into an sf object and set its CRS to WGS84
+ran_occ <- st_as_sf(ran_occ, coords = c("decimalLongitude", "decimalLatitude"))
+# set CRS to WGS84
+st_crs(ran_occ) <- 4326
 
-# crop the SpatVector to the extent of British Columbia
-ran_occ_bc <- crop(ran_occ_vect, bc_extent_rast)
-ran_occ_bc
+# crop the multiraster with predictors to the extent of interest
+predictors_multirast <- crop(predictors_multirast, bc_bound)
+
+# mask the multiraster to the extent
+predictors_mask <- mask(predictors_multirast, bc_bound)
 
 # plot the points on the raster that contains the climatic variables
 library(ggplot2)
 ggplot() +
-  geom_raster(data = pred_multirast, aes(fill = pred_multirast)) +
+  geom_raster(data = predictors_multirast, aes(fill = pred_multirast)) +
   geom_point(data = ran_occ_vect, aes(x = longitude, y = latitude), color = "green3")
