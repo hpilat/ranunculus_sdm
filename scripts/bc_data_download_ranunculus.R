df7d61d99dc3a46b7f9e1a9b3655d4be206be690
hpilat <hpilat@student.ubc.ca>
2024-01-21 12:02:06 -0600

    added new scripts with BC data so other scripts could be overwritten with NA data

---
+++ scripts/bc_data_download_ranunculus.R
@@ -0,0 +1,267 @@
+# dir.create("data/")
+# dir.create("scripts/")
+library(tidyverse)
+library(geodata) # won't work when on AAFC VPN
+library(bcmaps)
+library(bcdata)
+library(terra)
+library(raster)
+library(sf)
+library(rgbif)
+library(CoordinateCleaner)
+# note: cannot load rgdal and terra at the same time 
+# if using project function from terra (call terra::project)
+
+## Extent ##
+
+# read in the boundaries for British Columbia (BC)
+# bc_extent <- bcmaps::bc_bbox(class = ("raster"), crs = "EPSG:4326")
+
+# bc_bound_sf <- bcmaps::bc_bound(ask = interactive, force = FALSE)
+
+# create a SpatVector for the BC boundary
+# bc_bound <- vect(bc_bound_sf)
+
+# reproject BC Boundary to WGS84
+# bc_bound <- terra::project(bc_bound, "EPSG:4326")
+
+# create a SpatExtent for BC based on the bc_extent object
+# cannot coerce Extent object to SpatRaster
+# bc_spatextent <- ext(bc_extent)
+
+# create a vector from the BC boundary
+# bc_vect <- as.polygons(bc_spatextent, crs = "EPSG:4326")
+
+# now create a regular SpatRaster for bc_extent
+# bc_extent_rast <- rast(bc_spatextent)
+
+
+# read in administrative boundaries for Canada, USA, Mexico
+# north_america_bound <- geodata::gadm(country = c("CAN", "USA", "MEX"), 
+# level = 0, 
+# path = "data/", 
+# version = "latest", 
+# resolution = 1)
+
+# create an extent object so we can crop from the West Coast
+# north_america_ext <- ext(north_america_bound)
+
+# create an extent object so the admin boundaries can be cropped from 
+# the 100th meridian
+# general_extent <- ext(-140, -100, 25, 65)
+
+# first crop out the eastern portion
+# west_north_america <- crop(north_america_bound, general_extent)
+
+# now crop out Hawai'i and ocean (I think that's what I did here?)
+# west_na_ext <- crop(west_north_america, north_america_ext)
+
+
+
+## Spatial Extent ##
+na_bound <- read_sf("data/continental_divide_buffer_boundary.shp")
+na_bound <- vect(na_bound)
+na_extent <- ext(na_bound)
+
+
+## Occurrence Data ##
+
+# set up GBIF credentials
+# install.packages("usethis")
+# usethis::edit_r_environ()
+
+# download occurrence data for Ranunculus glaberrimus
+rgbif::occ_download(
+  pred("hasGeospatialIssue", FALSE), 
+  pred("hasCoordinate", TRUE), 
+  pred("basisOfRecord", "HUMAN_OBSERVATION"),
+  pred("country", "CA"),
+  pred("taxonKey", 3033299), 
+  format = "SIMPLE_CSV")
+
+# to check status of download:
+occ_download_wait('0019946-231120084113126')
+
+# to access download when it's finished
+ran_occ_download <- occ_download_get('0019946-231120084113126') %>%
+  occ_download_import()
+
+# Download Info:
+# Username: hpilat
+# E-mail: hannahepilat@gmail.com
+# Format: SIMPLE_CSV
+# Download key: 0019946-231120084113126
+# Created: 2023-12-05T18:28:14.357+00:00
+# Citation Info:  
+# Please always cite the download DOI when using this data.
+# https://www.gbif.org/citation-guidelines
+# DOI: 10.15468/dl.cwqmqu
+# Citation:
+#  GBIF Occurrence Download https://doi.org/10.15468/dl.cwqmqu Accessed from R 
+# via rgbif (https://github.com/ropensci/rgbif) on 2023-12-05
+
+## Predictor Data ##
+
+# read in BEC map from bcmaps
+# bc_bec <- bcmaps::bec(ask = interactive(), force = FALSE)
+
+# elevation data for North America
+elevation_na <- rast("data/northamerica_elevation_cec_2023.tif")
+
+# read in landcover data for North America
+lndcvr_na <- rast("data/NA_NALCMS_landcover_2020_30m.tif")
+
+# soil temperature data:
+soil_temp_0_5 <- rast("data/SBIO4_Temperature_Seasonality_0_5cm.tif")
+soil_temp_5_15 <- rast("data/SBIO4_Temperature_Seasonality_5_15cm.tif")
+
+# world soil pH data: 
+# had an error with geodata package, said server was down for maintenance
+# https://files.isric.org/soilgrids/latest/data_aggregated/1000m/phh2o/
+
+# soil pH data in case geodata package acts up:
+# soil_phh2o_0_5 <- rast("data/phh2o_0-5cm_mean_1000.tif")
+# soil_phh2o_5_15 <- rast("data/phh2o_5-15cm_mean_1000.tif")
+
+soil_phh2o_0_5 <- geodata::soil_world(var = "phh2o", depth = 5, stat = "mean", 
+                                      path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+                                      na.rm = TRUE)
+
+soil_phh2o_5_15 <- geodata::soil_world(var = "phh2o", depth = 15, stat = "mean", 
+                                       path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+                                       na.rm = TRUE)
+
+
+# unified North American soil data
+# download.file("https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1242", 
+# paste("C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data\\",
+# "unified_north_american_soil.tif", sep = ""), mode = "wb")
+
+# error with file type?
+# unified_soil <- rast("data/unified_north_american_soil.tif")
+
+# climate data, monthly averages from worldclim
+# average monthly temperature
+# temp_avg_can <- geodata::worldclim_country(country = "CAN", 
+#  var = "tavg", 
+#  path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+#  version = "2.1", 
+#  na.rm = TRUE)
+
+# temp_avg_usa <- geodata::worldclim_country(country = "USA", 
+#   var = "tavg", 
+#   path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+#   version = "2.1", 
+#   na.rm = TRUE)
+
+# temp_avg_mex <- geodata::worldclim_country(country = "MEX", 
+#  var = "tavg", 
+#  path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+#  version = "2.1", 
+#  na.rm = TRUE)
+
+# merge the temp_avg SpatRasters to create one for North America
+# temp_avg <- terra::merge(temp_avg_can, temp_avg_usa, first = TRUE, na.rm = TRUE)
+# temp_avg <- terra::merge(temp_avg, temp_avg_mex, first = TRUE, na.rm = TRUE)
+# temp_avg <- writeRaster(temp_avg, filename = "data/temp_avg_north_america.tif")
+temp_avg <- rast("data/temp_avg_north_america.tif")
+# plotting temp_avg = whole world for some reason?
+
+# total precipitation
+# precip_can <- geodata::worldclim_country(country = "CAN", 
+#  var = "prec", 
+#  path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+#  version = "2.1", 
+#  na.rm = TRUE)
+
+# precip_usa <- geodata::worldclim_country(country = "USA", 
+# var = "prec", 
+# path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+# version = "2.1", 
+# na.rm = TRUE)
+
+# precip_mex <- geodata::worldclim_country(country = "MEX", 
+# var = "prec", 
+# path = "C:\\Users\\PilatH\\OneDrive - AGR-AGR\\Documents\\ranunculus_sdm\\data", 
+# version = "2.1", 
+# na.rm = TRUE)
+
+# merge the individual precip SpatRasters into one for North America
+# precip <- terra::merge(precip_can, precip_usa, first = TRUE, na.rm = TRUE)
+# precip <- terra::merge(precip, precip_mex, first = TRUE, na.rm = TRUE)
+# precip <- writeRaster(precip, filename = "data/total_precip_na.tif")
+precip <- rast("data/total_precip_na.tif")
+# same as temp_avg, whole world plots
+
+# read in anthropogenic biome data
+anth_biome <- rast("data/anthromes_EqArea.tif")
+
+# read in protected areas data
+# IUCN categories:
+protect_area_IUCN <- shapefile("data/CEC_NA_2021_terrestrial_IUCN_categories.shp")
+protect_area_IUCN <- vect(protect_area_IUCN)
+protect_area_IUCN <- rast(protect_area_IUCN)
+# OECMs - what does this mean?
+protect_area_OECM <- system.file()
+protect_area_OECM <- vect(protect_area_OECM)
+protect_area_OECM <- rast(protect_area_OECM)
+
+
+
+
+
+
+# monthly snowpack, NetCDF file requires special software to download?
+# snowpack_canada <- rast("data/")
+
+# future climate predictions Shared Socioeconomic Pathway 126 (no climate policy?)
+cmip6_2021_2040_126 <- geodata::cmip6_tile(lon, lat, model, ssp = "126", time = "2021-2040", 
+                                           var = , res = 2.5, path = )
+cmip6_2041_2060_126 <- geodata::cmip6_tile(lon, lat, model, ssp = "126", time = "2041-2060", 
+                                           var = , res = 2.5, path = )
+cmip6_2061_2080_126 <- geodata::cmip6_tile(lon, lat, model, ssp = "126", time = "2061-2080", 
+                                           var = , res = 2.5, path = )
+
+# future climate predictions Shared Socioeconomic Pathway 245 (low/mid)
+cmip6_2021_2040_245 <- geodata::cmip6_tile(lon, lat, model, ssp = "245", time = "2021-2040", 
+                                           var = , res = 2.5, path = )
+cmip6_2041_2060_245 <- geodata::cmip6_tile(lon, lat, model, ssp = "245", time = "2041-2060", 
+                                           var = , res = 2.5, path = )
+cmip6_2061_2080_245 <- geodata::cmip6_tile(lon, lat, model, ssp = "245", time = "2061-2080", 
+                                           var = , res = 2.5, path = )
+
+# future climate predictions Shared Socioeconomic Pathway 370 (mid/high)
+cmip6_2021_2040_370 <- geodata::cmip6_tile(lon, lat, model, ssp = "370", time = "2021-2040", 
+                                           var = , res = 2.5, path = )
+cmip6_2041_2060_370 <- geodata::cmip6_tile(lon, lat, model, ssp = "370", time = "2041-2060", 
+                                           var = , res = 2.5, path = )
+cmip6_2061_2080_370 <- geodata::cmip6_tile(lon, lat, model, ssp = "370", time = "2061-2080", 
+                                           var = , res = 2.5, path = )
+
+# future climate predictions Shared Socioeconomic Pathway 545 (high)
+cmip6_2021_2040_545 <- geodata::cmip6_tile(lon, lat, model, ssp = "545", time = "2021-2040", 
+                                           var = , res = 2.5, path = )
+cmip6_2041_2060_545 <- geodata::cmip6_tile(lon, lat, model, ssp = "545", time = "2041-2060", 
+                                           var = , res = 2.5, path = )
+cmip6_2061_2080_545 <- geodata::cmip6_tile(lon, lat, model, ssp = "545", time = "2061-2080", 
+                                           var = , res = 2.5, path = )
+
+# future land use change data
+# SSP1, RCP19 from years 2020 to 2100
+lulc_2020_SSP1_RCP19 <- rast("data/")
+lulc_2025_SSP1_RCP19 <-
+  lulc_2030_SSP1_RCP19 <- 
+  lulc_2035_SSP1_RCP19 <-
+  lulc_2040_SSP1_RCP19 <- 
+  lulc_2045_SSP1_RCP19 <- 
+  lulc_2050_SSP1_RCP19 <- 
+  lulc_2055_SSP1_RCP19 <- 
+  lulc_2060_SSP1_RCP19 <- 
+  lulc_2065_SSP1_RCP19 <- 
+  lulc_2070_SSP1_RCP19 <- 
+  lulc_2075_SSP1_RCP19 <- 
+  lulc_2080_SSP1_RCP19 <- 
+  lulc_2085_SSP1_RCP19 <- 
+  lulc_2090_SSP1_RCP19 <- 
+  lulc_2095_SSP1_RCP19 <- 
+  lulc_2100_SSP1_RCP19 <- 
---
+++ scripts/bc_data_prep_ranunculus.R
@@ -0,0 +1,215 @@
+## Occurrence Data ##
+
+# clean the occurrence records using CoordinateCleaner package?
+# record-level tests
+
+# cl_coord_ran <- clean_coordinates(x = ran_occ_download, lon = "decimalLongitude", 
+#  lat = "decimalLatitude",
+#  species = "scientificName")
+
+# select only the relevant columns (ID column, longitude, latitude)?
+ran_occ <- dplyr::select(ran_occ_download, gbifID, 
+                         decimalLongitude, decimalLatitude) # dataframe
+
+# create a SpatVector object for the occurrence data?
+ran_occ_vect <- vect(ran_occ, geom = c("decimalLongitude", "decimalLatitude"), 
+                     crs = "EPSG:4326", keepgeom = FALSE)
+
+# crop the SpatVector to the extent of British Columbia
+ran_occ_bc <- crop(ran_occ_vect, bc_bound)
+ran_occ_bc
+
+ran_occ_bc_sf <- st_as_sf(ran_occ_bc, 
+                          coords = c("decimalLongitude", "decimalLatitude"))
+ran_occ_bc_sf
+
+## Predictor Data ##
+
+# crop soil temperature SpatRaster to British Columbia extent
+soil_temp_0_5_bc <- crop(soil_temp_0_5, bc_bound)
+soil_temp_5_15_bc <- crop(soil_temp_5_15, bc_bound)
+
+# reproject soil temperature SpatRasters to WGS84
+soil_temp_0_5_bc <- terra::project(soil_temp_0_5_bc, "EPSG:4326",
+                                   method = "bilinear")
+soil_temp_5_15_bc <- terra::project(soil_temp_5_15_bc, "EPSG:4326",
+                                    method = "bilinear")
+
+# crop pH SpatRaster to British Columbia extent
+soil_phh2o_0_5_bc <- crop(soil_phh2o_0_5, bc_bound)
+soil_phh2o_5_15_bc <- crop(soil_phh2o_5_15, bc_bound)
+
+# reproject soil pH SpatRasters to WGS84
+soil_phh2o_0_5_bc <- terra::project(soil_temp_0_5_bc, "EPSG:4326",
+                                    method = "bilinear")
+soil_phh2o_5_15_bc <- terra::project(soil_temp_5_15_bc, "EPSG:4326",
+                                     method = "bilinear")
+
+# create a SpatRaster of the BEC data
+# start by turning bc_bec into a SpatVector object
+# bc_bec_vec <- vect(bc_bec)
+
+# calculate number of rows (Y direction) and columns (X) for raster 
+# using 1km resolution (1000)
+# numcols <- as.vector(ceiling((st_bbox(bc_bec)$xmax - st_bbox(bc_bec)$xmin)/1000))
+# numrows <- as.vector(ceiling((st_bbox(bc_bec)$ymax - st_bbox(bc_bec)$ymin)/1000))
+
+# create a temporary raster with number of columns and rows from above
+# bec_temprast <- rast(bc_bec_vec, ncols = numcols, nrows = numrows)
+
+# create raster from SpatVector and structure of temporary raster
+# select "ZONE" layer from BEC data
+# bc_bec <- rasterize(bc_bec_vec, bec_temprast, "ZONE")
+# plot(bc_bec)
+
+# reproject BEC data to WGS84
+# bc_bec <- terra::project(bc_bec, "EPSG:4326", method = "near")
+
+# crop BEC data to match BC extent?
+# bc_bec <- crop(bc_bec, bc_bound)
+
+# resample BEC data to match resolution of other rasters
+# bc_bec <- resample(bc_bec, soil_temp_0_5_bc)
+
+# write new BEC data to raster for easier future use
+# bc_bec <- writeRaster(bc_bec, "data/bc_bec.tif", overwrite = TRUE)
+
+# import bc_bec data from new file
+bc_bec <- rast("data/bc_bec.tif")
+
+# crop elevation data to British Columbia extent
+elevation_bc <- crop(elevation_canada, bc_bound)
+
+# reproject elevation data to WGS84
+elevation_bc <- terra::project(elevation_bc, "EPSG:4326", method = "bilinear")
+
+# select March to June for average temperatures (relevant to growing season)
+tavg_canada_mar_jun <- tavg_canada[[3:6]]
+
+tavg_canada_mar_jun <- project(tavg_canada_mar_jun, "EPSG:4326", method = "bilinear")
+
+# OR: 
+# aggregate tavg_canada raster so it can be cropped?
+# by a factor of 3 to go from 5040 columns to closer to 1404 (size of bc_extent_rast)
+# agg_tavg_canada <- aggregate(tavg_canada, fact = 3)
+# tavg_bc <- crop(tavg_canada, bc_extent_rast)
+# resample average monthly temperature data
+# tavg_bc <- resample(agg_tavg_canada, soil_temp_0_5_bc)
+
+# crop average monthly temperature data to British Columbia extent
+tavg_bc <- crop(tavg_canada_mar_jun, bc_bound)
+
+# crop precipitation data to British Columbia extent
+prec_bc <- crop(prec_canada, bc_bound)
+
+# aggregate landcover data so it can be reprojected and cropped
+# lndcvr_na_agg <- aggregate(lndcvr_na, fact = 15)
+
+# write aggregated landcover data to file for easier reuse
+# lndcvr_na_agg <-writeRaster(lndcvr_na_agg, 
+# filename = "data/lndcvr-north-america_agg.tif", 
+# overwrite = TRUE)
+
+# create SpatRaster of aggregated landcover data from new file
+# lndcvr_na_agg <- rast("data/lndcvr-north-america_agg.tif")
+
+# reproject landcover North America data to WGS84
+# lndcvr_na_agg <- terra::project(lndcvr_na_agg, "EPSG:4326", method = "near")
+
+# crop landcover North America data to BC's extent
+# lndcvr_bc <- crop(lndcvr_na_agg, bc_bound)
+
+# resample landcover BC data to change resolution
+# lndcvr_bc <- resample(lndcvr_bc, soil_temp_0_5_bc)
+
+# create file of BC landcover data for easier/faster re-use
+# lndcvr_bc <- writeRaster(lndcvr_bc, "data/lndcvr_bc.tif", overwrite = TRUE)
+
+# import landcover BC data from new file created above
+lndcvr_bc <- rast("data/lndcvr_bc.tif")
+
+# mask all layers so values outside of bc_bound are NA
+elevation_bc <- mask(elevation_bc, bc_bound)
+soil_temp_0_5_bc <- mask(soil_temp_0_5_bc, bc_bound)
+soil_temp_5_15_bc <- mask(soil_temp_5_15_bc, bc_bound)
+soil_phh2o_0_5_bc <- mask(soil_phh2o_0_5_bc, bc_bound)
+soil_phh2o_5_15_bc <- mask(soil_phh2o_5_15_bc, bc_bound)
+prec_bc <- mask(prec_bc, bc_bound)
+bc_bec <- mask(bc_bec, bc_bound)
+tavg_bc <- mask(tavg_bc, bc_bound)
+lndcvr_bc <- mask(lndcvr_bc, bc_bound)
+
+# trim NA values from outside of BC boundary
+elevation_bc <- trim(elevation_bc, padding = 0, value = NA)
+soil_temp_0_5_bc <- trim(soil_temp_0_5_bc, padding = 0, value = NA)
+soil_temp_5_15_bc <- trim(soil_temp_5_15_bc)
+soil_phh2o_0_5_bc <- trim(soil_phh2o_0_5_bc, padding = 0, value = NA)
+soil_phh2o_5_15_bc <- trim(soil_phh2o_5_15_bc, padding = 0, value = NA)
+prec_bc <- trim(prec_bc, padding = 0, value = NA)
+bc_bec <- trim(bc_bec, padding = 0, value = NA)
+tavg_bc <- trim(tavg_bc, padding = 0, value = NA)
+lndcvr_bc <- trim(lndcvr_bc, padding = 0, value = NA)
+
+# set all remaining NA values to -9999 (predictors cannot have NA values)
+elevation_bc[is.na(elevation_bc)] <- -500
+soil_temp_0_5_bc[is.na(soil_temp_0_5_bc)] <- -100
+soil_temp_5_15_bc[is.na(soil_temp_5_15_bc)] <- -100
+soil_phh2o_0_5_bc[is.na(soil_phh2o_5_15_bc)] <- -10
+soil_phh2o_5_15_bc[is.na(soil_phh2o_5_15_bc)] <- -10
+bc_bec[is.na(bc_bec)] <- -1
+lndcvr_bc[is.na(lndcvr_bc)] <- -1
+
+# prec_bc:
+for(i in 1:12){
+  prec_bc[is.na(prec_bc[,,i])] <- -100
+}
+
+#tavg_bc
+for(i in 1:4){
+  tavg_bc[is.na(tavg_bc[,,i])] <- -100
+}
+
+# extents moved around a bit - need to crop to smallest extent (bc_bec or lndcvr_bc)
+elevation_bc <- crop(elevation_bc, bc_bound)
+soil_temp_0_5_bc <- crop(soil_temp_0_5_bc, bc_bound)
+soil_temp_5_15_bc <- crop(soil_temp_5_15_bc, bc_bound)
+soil_phh2o_0_5_bc <- crop(soil_phh2o_0_5_bc, bc_bound)
+soil_phh2o_5_15_bc <- crop(soil_phh2o_5_15_bc, bc_bound)
+prec_bc <- crop(prec_bc, bc_bound)
+tavg_bc <- crop(tavg_bc, bc_bound)
+lndcvr_bc <- crop(lndcvr_bc, bc_bound)
+
+
+# write rasters to file for faster computation
+elevation_bc <- writeRaster(elevation_bc, filename = "data/elevation_bc.tif")
+soil_temp_0_5_bc <- writeRaster(soil_temp_0_5_bc, 
+                                filename = "data/soil_temp_0_5_bc.tif")
+soil_temp_5_15_bc <- writeRaster(soil_temp_5_15_bc, 
+                                 filename = "data/soil_temp_5_15_bc.tif")
+soil_phh2o_0_5_bc <- writeRaster(soil_phh2o_0_5_bc, 
+                                 filename = "data/soil_phh2o_0_5_bc.tif")
+soil_phh2o_5_15_bc <- writeRaster(soil_phh2o_5_15_bc,
+                                  filename = "data/soil_phh2o_5_15_bc.tif")
+prec_bc <- writeRaster(prec_bc, filename = "data/prec_bc.tif")
+bc_bec <- writeRaster(bc_bec, filename = "data/bc_bec.tif", overwrite = TRUE)
+tavg_bc <- writeRaster(tavg_bc, filename = "data/tavg_bc.tif")
+lndcvr_bc <- writeRaster(lndcvr_bc, filename = "data/lndcvr_bc.tif", overwrite = TRUE)
+
+
+## Multilayer Raster ##
+
+# create a multilayer raster of the predictor variables
+# new error: says extents don't match
+predictors_multirast <- rast(c(elevation_bc,
+                               soil_temp_0_5_bc, 
+                               soil_temp_5_15_bc,
+                               soil_phh2o_0_5_bc,
+                               soil_phh2o_5_15_bc,
+                               prec_bc,
+                               bc_bec,
+                               tavg_bc,
+                               lndcvr_bc))
+predictors_multirast <- writeRaster(predictors_multirast, filename = "data/predictors_multirast.tif")
+
+
+
